628b755c4c8b9c91e915b098305a85a2d4c61ea8
Jason Zhang <zhang98@bu.edu>
2021-01-17 00:43:42 -0500

    Connected mongoDB database

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try
+    {
+        // wait for promise to return value
+        await mongoose.connect(db, {useNewUrlParser:true, useUnifiedTopology: true });
+
+        // print to console that db is connected
+        console.log("mongoDB successfully connected");
+    }
+    catch(err)
+    {
+        // if error log the error
+        console.log(err.message);
+
+        // exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
