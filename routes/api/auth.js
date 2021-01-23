dd9b590e9481d06ca7664fe5dc8fc269dbdc5296
Jason Zhang <zhang98@bu.edu>
2021-01-22 21:06:09 -0500

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,24 +1,81 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const jwtSecret = config.get('jwtSecret');
+const {check, validationResult} = require('express-validator');
 
 const User = require('../../models/User');
 
 // @route - GET api/auth
-// @desc - test route
+// @desc - Authenticate user and get token
 // @access - public, no token needed
-router.get(('/'), auth, async (req, res) => {
+router.post(('/'), [
+
+    // params('field name', 'error message or default used')
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password').exists()
+],
+async (req, res) => {
+    // check to see if the request returned any errors
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty())
+    {
+        // return 400 - bad status and include with it json that contains the error
+        // errors.array will use custom error messages from above or if not provided, will use defualts
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
 
     try
     {
-        const user = await User.findById(req.user.id).select('-password');
+        // checks to see if db has a user with the email that the user entered
+        let user = await User.findOne({ email: email }).exec();
 
-        // No errors, send user back with user specific data
-        res.json(user);
+        // see if user exists based on email
+        if(!user)
+        {
+            return res.status(400).json({ errors: [{msg: 'No user found, please try again'}] });
+        }
+
+        // checks to see if password that user entered matches the one in db
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch)
+        {
+            // should make error messages same as above if you want added security so user doesn't know what is incorrect
+            // ie no user found or credentials are wrong
+            return res.status(400).json({ errors: [{msg: 'Username or password is incorrect'}] });
+        }
+
+
+        // return jwt
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            jwtSecret, 
+            {expiresIn: 360000}, 
+            (err, token) => {
+                if(err)
+                {
+                    throw err;
+                }
+
+                // sign-up successful
+                res.json({ token });
+            });
     }
     catch(err)
     {
-        // error, print it out
         console.error(err.message);
         res.status(500).send('Server error');
     }
